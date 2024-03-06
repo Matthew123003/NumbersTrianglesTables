0dc23eee2f5d95952d1365d505dc031bb50a95e2
Matt Kramer <matthew123003@gmail.com>
2024-03-06 08:14:08 -0500

    Finished

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -3,18 +3,36 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
         StringBuilder sb = new StringBuilder();
-        int n = 1;
-        for(int i = 0; i < n; i++) {
-            sb.append(i);
+        int n = 5;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++){
+                int k = i * j;
+                if(k < 10){
+                    sb.append("  " + k + " ").append("|");
+                }else if(k >= 10 && k <= 25){
+                    sb.append(" " + k + " " ).append("|");
+                }
+            }
+            sb.append("\n");
         }
         return sb.toString();
     }
 
     public static String getLargeMultiplicationTable() {
         StringBuilder sb = new StringBuilder();
-        int n = 1;
-        for(int i = 0; i < n; i++) {
-            sb.append(i);
+        int n = 10;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++){
+                int k = i * j;
+                if(k < 10){
+                    sb.append("  " + k + " ").append("|");
+                } else if (k >= 10 && k < 100){
+                    sb.append(" " + k + " ").append("|");
+                }else{
+                    sb.append(k + " ").append("|");
+                }
+            }
+            sb.append("\n");
         }
         return sb.toString();
     }
