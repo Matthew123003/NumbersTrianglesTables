59adcd330878b0776d5a5be7a3a2830e649b4929
Matt Kramer <matthew123003@gmail.com>
2024-03-06 07:53:09 -0500

    Got last table right

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,18 +2,37 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        int n = 1;
+        for(int i = 0; i < n; i++) {
+            sb.append(i);
+        }
+        return sb.toString();
     }
 
     public static String getLargeMultiplicationTable() {
-
-        return null;
+        StringBuilder sb = new StringBuilder();
+        int n = 1;
+        for(int i = 0; i < n; i++) {
+            sb.append(i);
+        }
+        return sb.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < tableSize; i++){
-            sb.append(i);
+        for(int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                int k = i * j;
+                if(k < 10){
+                    sb.append("  "+k+" ").append("|");
+                } else if (k >= 10 && k < 100) {
+                    sb.append(" "+k+" ").append("|");
+                } else if (k >= 100 && k <= 400){
+                    sb.append(k+" ").append("|");
+                }
+            }
+            sb.append("\n");
         }
         return sb.toString();
     }
